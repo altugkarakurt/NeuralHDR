ae990b266a134e34164a99d5dd8e83a3024ee9e9
alpernebbi <alpernebiyasak@gmail.com>
2015-11-08 15:17:55 +0200

    Single perceptron class

---
+++ Experimental/Perceptron.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import numpy
+from numpy import sign as signum
+
+def heaviside(x): return (0.5 * (numpy.sign(x) + 1))
+
+class Perceptron:
+	def __init__(self, *weights, activation_function=heaviside):
+		self.activation_function = activation_function
+		self.weights = numpy.array(weights)
+	
+	def __call__(self, *inputs):
+		inputs = numpy.array(inputs)
+		local_field = self.weights @ inputs;
+		return self.activation_function(local_field)
