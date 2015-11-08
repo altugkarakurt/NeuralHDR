87cfa72689792d3ea9d8d2b06e913a14965006a0
alpernebbi <alpernebiyasak@gmail.com>
2015-11-08 15:45:25 +0200

    Change heaviside definition

--- Experimental/Perceptron.py
+++ Experimental/Perceptron.py
@@ -3,7 +3,8 @@
 import numpy
 from numpy import sign as signum
 
-def heaviside(x): return (0.5 * (numpy.sign(x) + 1))
+# def heaviside(x): return (0.5 * (numpy.sign(x) + 1))
+def heaviside(x): return (1 if x >= 0 else 0)
 
 class Perceptron:
 	def __init__(self, *weights, activation_function=heaviside):
