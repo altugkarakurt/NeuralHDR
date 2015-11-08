4133d5556dca882896125a6241c7d94d71d9cbbb
altug <altugkarakurt@gmail.com>
2015-11-08 14:39:15 +0200

    MLP estimation backbone

--- MLP.py
+++ MLP.py
@@ -10,5 +10,11 @@ class MLP:
 		self.weights = [randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 
 	def estimate(self, digit):
+		x_l = digit
+		for l, l_size in enumerate(layer_sizes[1:]):
+			y_l = np.zeros(l_size)
+			for i in xrange(l_size)
+				local_field = np.dot(self.weights[l][i], x_l) + self.biases[l]
+				x_l[i] = activation_function(local_field)
 
 	def train(self, labels, digits):
