5ccb82be2fd008ad49a2b1dd0ba7e6addacb32eb
alpernebbi <alpernebiyasak@gmail.com>
2015-11-29 19:42:03 +0200

    Combine biases and weights

--- MLP.py
+++ MLP.py
@@ -13,10 +13,9 @@ def d_sigmoid(x): return sigmoid(x) * (1 - sigmoid(x))
 
 class MLP:
 
-	def __init__(self, sizes, activation_function=(sigmoid, d_sigmoid), biases=None, weights=None):
+	def __init__(self, sizes, activation_function=(sigmoid, d_sigmoid), weights=None):
 		# TODO: check whether bias, weight sizes are compatible with layer sizes
-		self.sizes = sizes
-		self.biases = np.array(biases) if biases is not None else [randn(y, 1) for y in sizes[1:]] 
+		self.sizes = np.array(sizes)
 		self.weights = np.array(weights) if weights is not None else [randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
 		self.activation_function = activation_function[0]
 		self.d_activation_function = activation_function[1]
